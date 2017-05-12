c6bd54c9944820bd0994b139c1652ae1b39e1d10
Alexander <fm107@ya.ru>
2017-05-12 13:10:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,21 @@ sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotne
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get -y update
 
-sudo apt-get install -y dotnet-dev-1.0.4
+sudo apt-get install -y dotnet-dev-1.0.4 nodejs npm  libunwind8 gettext
+
+git clone -b Lastest --single-branch  https://github.com/fm107/testing.git
+
+sudo npm install -g bower
+
+sudo mkdir -p /app/heroku_output/ && sudo chown -R 1000:1000 /app
+
+cd testing
+
+dotnet restore
+dotnet publish WebTorrent.csproj --output /app/heroku_output --configuration Release
+
+curl --silent --location http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-ubuntu-13-04 | tar xz
+mv utorrent-server-alpha-v3_3 /app/utorrent-server
+curl -o /app/utorrent-server/utserver.conf https://raw.githubusercontent.com/fm107/heroku-buildpack-utorrent/master/bin/utserver.conf
+ 
+cd /app/heroku_output && sudo dotnet ./WebTorrent.dll --server.urls http://+:\80
