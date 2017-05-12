a24da631169da1dacc07a54ea9605da54bbde9ef
Alexander <fm107@ya.ru>
2017-05-12 14:22:09 +0300

    Update re-publish.sh

--- re-publish.sh
+++ re-publish.sh
@@ -3,6 +3,7 @@
 rm -rf /home/ubuntu/testing /app/heroku_output
 
 git clone -b Lastest --single-branch https://github.com/fm107/testing.git
+cd testing
 dotnet restore
 dotnet publish WebTorrent.csproj --output /app/heroku_output --configuration Release
 cd /app/heroku_output && sudo dotnet ./WebTorrent.dll --server.urls http://+:\80
