4b0e3e95a71e0b30bc9c233c567a24d5063c7f8b
Alexander <fm107@ya.ru>
2017-05-12 11:18:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-sudo apt-get update
+sudo apt-get -y update
 
 sudo apt-get install -y dotnet-dev-1.0.4
